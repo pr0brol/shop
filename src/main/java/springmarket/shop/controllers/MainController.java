81eee074460012eaaa9c3bc1bf041076515487a0
pr0brol <pr0_brol@mail.ru>
2019-12-20 10:58:13 +0300

    hw2-1

--- src/main/java/springmarket/shop/controllers/MainController.java
+++ src/main/java/springmarket/shop/controllers/MainController.java
@@ -1,22 +1,25 @@
 package springmarket.shop.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import springmarket.shop.entities.Category;
+import springmarket.shop.entities.Order;
 import springmarket.shop.entities.Product;
 import springmarket.shop.entities.User;
 import springmarket.shop.services.CategoryService;
+import springmarket.shop.services.OrderService;
 import springmarket.shop.services.ProductService;
 import springmarket.shop.services.UserService;
 import springmarket.shop.utils.Cart;
 import springmarket.shop.utils.ProductFilter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
@@ -26,13 +29,14 @@ public class MainController {
     private ProductService productService;
     private CategoryService categoryService;
     private UserService userService;
+    private OrderService orderService;
     private Cart cart;
 
-    @Autowired
-    public MainController(ProductService productService, CategoryService categoryService, UserService userService, Cart cart) {
+    public MainController(ProductService productService, CategoryService categoryService, UserService userService, OrderService orderService, Cart cart) {
         this.productService = productService;
         this.categoryService = categoryService;
         this.userService = userService;
+        this.orderService = orderService;
         this.cart = cart;
     }
 
@@ -56,10 +60,6 @@ public class MainController {
         int pageIndex = 0;
         if (params.containsKey("p")) {
             pageIndex = Integer.parseInt(params.get("p")) - 1;
-        }
-        if(params.containsKey("id") && !params.get("id").isEmpty()){
-            Long id = Long.parseLong(params.get("id"));
-            cart.add(id);
         }
         Pageable pageRequest = PageRequest.of(pageIndex, 3);
         ProductFilter productFilter = new ProductFilter(params);
@@ -90,16 +90,41 @@ public class MainController {
 
     @GetMapping("/cart")
     public String cartForm(Model model) {
-        Map<Product, Integer> products = cart.allProductsInCart();
-        int count = 0;
-        int sum = 0;
-        for (Map.Entry<Product, Integer> prod: products.entrySet()) {
-            sum += prod.getKey().getPrice() * prod.getValue();
-            count += prod.getValue();
-        }
-        model.addAttribute("products", products);
-        model.addAttribute("sum", sum);
-        model.addAttribute("count", count);
+        model.addAttribute("cart", cart);
         return "cart";
+    }
+
+    @GetMapping("/cart/add/{id}")
+    public void addProductToCart(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        cart.add(productService.findById(id));
+        response.sendRedirect(request.getHeader("referer"));
+    }
+
+    @GetMapping("/orders/control")
+    public String showOrder(Model model, Principal principal){
+        User user = userService.findByPhone(principal.getName());
+        model.addAttribute("user", user);
+        model.addAttribute("cart", cart);
+        return "save_order";
+    }
+
+    @GetMapping("/orders/create")
+    public String createOrder(Principal principal){
+        User user = userService.findByPhone(principal.getName());
+        Order order = new Order(user, cart);
+        orderService.save(order);
+        return "redirect:/";
+    }
+
+    @GetMapping("/cart/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        cart.removeById(id);
+        return "redirect:/cart";
+    }
+
+    @GetMapping("/orders/control/delete/{id}")
+    public String deleteProductFromOrder(@PathVariable Long id){
+        cart.removeById(id);
+        return "redirect:/orders/control";
     }
 }
