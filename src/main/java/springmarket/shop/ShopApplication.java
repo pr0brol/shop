2827979283923e2682081d8c54e4e9b9ab77d443
pr0brol <pr0_brol@mail.ru>
2020-02-26 18:49:33 +0300

    hw-3

--- src/main/java/springmarket/shop/ShopApplication.java
+++ src/main/java/springmarket/shop/ShopApplication.java
@@ -3,8 +3,29 @@ package springmarket.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ShopApplication {
+
+
+	// План на курс:
+	// 1. Добавить платежную систему (PayPal)
+	// 2. Авторизация через соцсети
+	// 4. Отправка уведомлений пользователю, на сайте, или на почту
+	// 5. Промокоды
+	// 6. Логирование
+	// 7. Профиль/редактирование профиля
+	// 8. Отдельная админка
+	// 9. Картинки для товаров
+	// 10. HTTPS
+	// 11. История просмотров товара (куки)
+	// 12. Статистика для владельца
+	// 13. История действий на сайте
+	// 14. Смс сервис
+	// 15. Восстановление пароля
+	// 16. Формирование PDF для заказа
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
